7e08678637ed7a910216ee5fbb6c77eadad1aee0
asungkim <asungkim007@naver.com>
2025-01-06 11:17:49 +0900

    t8 blue

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,28 +3,28 @@ import java.util.Scanner;
 public class App {
 
     private final Scanner sc;
+    private int lastId;
 
     public App(Scanner sc) {
         this.sc = sc;
+        this.lastId = 0;
     }
 
     public void run() {
         System.out.println("== 명언 앱 ==");
 
-
         while (true) {
             System.out.println("명령) ");
-            String cmd=sc.nextLine();
+            String cmd = sc.nextLine();
+
             if (cmd.equals("종료")) {
                 System.out.println("명언앱을 종료합니다.");
                 break;
-            }
-            else if (cmd.equals("등록")) {
+            } else if (cmd.equals("등록")) {
                 System.out.println("명언 : ");
                 System.out.println("작가 : ");
 
-                System.out.println("1번 명언이 등록되었습니다.");
-                System.out.println("2번 명언이 등록되었습니다.");
+                System.out.println("%d번 명언이 등록되었습니다.".formatted(++lastId));
             }
         }
 
