5359d9fe1b6470361f6d75f716eeb30c8a6e55da
asungkim <asungkim007@naver.com>
2025-01-06 23:49:51 +0900

    file t3 red

--- src/main/java/app/standard/Util.java
+++ src/main/java/app/standard/Util.java
@@ -1,5 +1,10 @@
 package app.standard;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Util {
 
     public static class File {
@@ -7,8 +12,20 @@ public class Util {
             System.out.println("파일 유틸 테스트");
         }
 
-        public static void createFile() {
+        public static void createFile(String fileName)  {
+            Path filePath= Paths.get(fileName);
 
+            try {
+                Files.createFile(filePath);
+            }
+            catch (Exception e) {
+                System.out.println("파일 생성 실패");
+                e.printStackTrace();
+            }
+        }
+
+        public static String readAsString(String file) {
+            return null;
         }
     }
 
--- src/test/java/app/standard/FIleTest.java
+++ src/test/java/app/standard/FIleTest.java
@@ -4,10 +4,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FIleTest {
@@ -19,16 +21,37 @@ public class FIleTest {
 
     @Test
     @DisplayName("빈 파일 생성")
-    void t2() {
+    void t2()  {
 
-        String fileName="test.txt";
-        Path filePath= Paths.get(fileName);
+        String file="test.txt";
+        Util.File.createFile(file); // 파일 생성 ok
 
-        Util.File.createFile(); // 파일 생성 ok
-
-        Assertions.assertThat(Files.exists(filePath)).isTrue();
-
+        assertThat(Files.exists(Paths.get(file)))
+                .isTrue();
 
     }
+
+    @Test
+    @DisplayName("파일 내용 읽어오기")
+    void t3() {
+        String file="test.txt";
+
+        String testContent="Hello World!";
+
+        String content= Util.File.readAsString(file);
+
+        assertThat(content).isEqualTo(testContent);
+    }
+
+    //    @Test
+//    @DisplayName("파일 내용 수정")
+//    void t4() {
+//        String file="test.txt";
+//        Path filePath = Paths.get(file);
+//
+//        Util.File.write(file,"Hello World!");
+//
+//
+//    }
 
 }
