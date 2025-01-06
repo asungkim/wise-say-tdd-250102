ae8c83eb81b07cbfcd1459e75b53bb30c5f47cf6
asungkim <asungkim007@naver.com>
2025-01-06 10:49:14 +0900

    t7 r

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -13,25 +13,43 @@ public class FirstTest {
 
     @Test
     void t2() {
-        App app = new App();
-        app.run();
+//        App app = new App();
+//        app.run();
     }
 
     @Test
+    @DisplayName("명령에 종료를 입력하면 종료")
     void t3() {
         TestBot testBot = new TestBot();
-        String out = testBot.run("종료");
+        String out = testBot.run("");
 
-        assertThat(out).contains("명언앱을 종료합니다");
+        assertThat(out)
+                .contains("명령)")
+                .contains("명언앱을 종료합니다");
+
+    }
+
+    @Test
+    @DisplayName("명령을 여러번 입력할 수 있다.")
+    void t4() {
+        TestBot testBot = new TestBot();
+        String out = testBot.run("""
+                등록
+                """);
+
+        // 명령) 횟수를 계산
+        int cnt = out.split("명령\\)").length - 1;
+
+        assertThat(cnt).isEqualTo(2);
 
 
     }
 
     @Test
     @DisplayName("앱 시작시 '== 명언 앱 ==' 출력")
-    void t4() {
+    void t5() {
         TestBot testBot = new TestBot();
-        String out = testBot.run("종료");
+        String out = testBot.run("");
 
 
         assertThat(out)
@@ -42,18 +60,32 @@ public class FirstTest {
 
     @Test
     @DisplayName("등록 - 명언 1개 입력")
-    void t5() {
+    void t6() {
         TestBot testBot = new TestBot();
         String out = testBot.run("""
                 등록
                 현재를 사랑하라.
                 작자 미상
-                종료
                 """);
 
         assertThat(out)
-                .containsSubsequence("명언 : ","작가 : ");
+                .containsSubsequence("명언 : ", "작가 : ");
 
+
+    }
+
+    @Test
+    @DisplayName("등록시 명언번호 출력")
+    void t7() {
+        TestBot testBot = new TestBot();
+        String out = testBot.run("""
+                등록
+                현재를 사랑하라.
+                작자 미상
+                """);
+
+        assertThat(out)
+                .contains("1번 명언이 등록되었습니다.");
 
     }
 }
