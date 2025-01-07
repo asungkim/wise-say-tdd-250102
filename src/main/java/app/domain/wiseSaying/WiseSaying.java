54e4f7a60813389b0064fe5b5c80881e95574dd5
asungkim <asungkim007@naver.com>
2025-01-07 11:34:19 +0900

    json t4 g

--- src/main/java/app/domain/wiseSaying/WiseSaying.java
+++ src/main/java/app/domain/wiseSaying/WiseSaying.java
@@ -3,15 +3,23 @@ package app.domain.wiseSaying;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 @Getter @Setter
 public class WiseSaying {
 
     private int id;
 
-    private String author;
-
     private String content;
 
+    private String author;
+
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
 
     public WiseSaying(String content, String author) {
         this.content = content;
@@ -25,5 +33,14 @@ public class WiseSaying {
 
     public boolean isNew() {
         return this.id == 0;
+    }
+
+    public Map<String,Object> toMap() {
+        Map<String,Object> map=new LinkedHashMap<>();
+        map.put("id",id);
+        map.put("content",content);
+        map.put("author",author);
+
+        return map;
     }
 }
