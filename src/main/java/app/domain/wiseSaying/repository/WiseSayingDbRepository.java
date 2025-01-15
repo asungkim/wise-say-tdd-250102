a6fcca1b29ef2ab5d3c0cb096dba8610038da24a
asungkim <asungkim007@naver.com>
2025-01-15 11:34:21 +0900

    test preprocess

---
+++ src/main/java/app/domain/wiseSaying/repository/WiseSayingDbRepository.java
@@ -0,0 +1,33 @@
+package app.domain.wiseSaying.repository;
+
+import app.domain.wiseSaying.WiseSaying;
+import app.standard.simpleDb.SimpleDb;
+import app.standard.simpleDb.Sql;
+
+public class WiseSayingDbRepository {
+    private final SimpleDb simpleDb;
+
+    public WiseSayingDbRepository() {
+        this.simpleDb = new SimpleDb("localhost", "root", "lldj123414", "wiseSaying__test");
+    }
+
+    public WiseSaying save(WiseSaying wiseSaying) {
+        return null;
+    }
+
+    public void createWiseSayingTable() {
+        simpleDb.run("DROP TABLE IF EXISTS wise_saying");
+
+        simpleDb.run("""
+                CREATE TABLE wise_saying (
+                    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                    content VARCHAR(100) NOT NULL,
+                    author VARCHAR(100) NOT NULL
+                )
+                """);
+    }
+
+    public void truncateWiseSayingTable() {
+        simpleDb.run("TRUNCATE TABLE wise_saying");
+    }
+}
