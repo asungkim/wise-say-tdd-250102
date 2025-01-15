696fae247301ab1055f4fb5394fce54bab281976
asungkim <asungkim007@naver.com>
2025-01-15 15:50:32 +0900

    mysql db mode add

--- src/main/java/app/domain/wiseSaying/repository/RepositoryProvider.java
+++ src/main/java/app/domain/wiseSaying/repository/RepositoryProvider.java
@@ -4,13 +4,11 @@ import app.global.AppConfig;
 
 public class RepositoryProvider {
     public static WiseSayingRepository provide() {
-//        if (AppConfig.isFileDb()) {
-//            return new WiseSayingFileRepository();
-//        } else {
-//            return new WiseSayingMemRepository();
-//        }
-//            return new WiseSayingFileRepository();
+        if (AppConfig.isFileDb()) {
+            return new WiseSayingFileRepository();
+        }
+        else if (AppConfig.isMysqlDb()) return new WiseSayingDbRepository();
 
-        return new WiseSayingDbRepository();
+        throw new RuntimeException("지원하지 않는 DB 타입");
     }
 }
